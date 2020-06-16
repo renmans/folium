d42b62944e570a4c84e5ef2d8f71cffe8dd78b6e
Renat <wiredrat@protonmail.com>
2020-06-16 17:31:21 +0300

    Add login form

--- app/app.py
+++ app/app.py
@@ -1,15 +1,19 @@
-import os
+import requests
 
-from flask import Flask, session, render_template
+from yaml import safe_load
+
+from forms import LoginForm
+
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+# Load configuration
+with open("../.env/config.yaml", "r") as f:
+    conf = safe_load(f)
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -17,13 +21,18 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(conf["database"]["URI"])
 db = scoped_session(sessionmaker(bind=engine))
 
+# Configure secret key
+app.config["SECRET_KEY"] = conf["SECRET_KEY"]
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        pass
+    return render_template("index.html", title="Sign Up", form=LoginForm())
 
 
 if __name__ == "__main__":
---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', [DataRequired(), Length(min=6, max=35)])
+    username = StringField('Username', [DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Password',
+                             [DataRequired(), Length(min=6, max=40)])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign Up')
